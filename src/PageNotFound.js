72abe9f8260a454d4acb7e9f57d87edddcf93869
shivateja18 <shivateja1805@gmail.com>
2024-04-23 16:23:51 +0530

    Modified pagenot found

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './Components/Navbar';
+import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
   return (
     <div>
       <center>
-        <h1>Page Not Found</h1>
+        <Navbar />
+        <div className='container d-flex flex-column justify-content-center align-item-center' style={{height:'80svh'}}>
+        <h1 style={{fontSize:'8vmin'}} >Page Not Found</h1>
         <h1>ERROR 404</h1>
+        <Link to={'/RestaurentMenu'}><button className='btn btn-warning'>Back to Home</button></Link>
+        </div>
       </center>
     </div>
   )
