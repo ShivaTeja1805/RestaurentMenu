0d6c967ee3ce7e6840a0d51fe466db3318f6618f
shivateja18 <shivateja1805@gmail.com>
2024-04-25 14:57:34 +0530

    modified PageNotFound

--- src/PageNotFound.js
+++ src/PageNotFound.js
@@ -10,7 +10,7 @@ const PageNotFound = () => {
         <div className='container d-flex flex-column justify-content-center align-item-center' style={{height:'80svh'}}>
         <h1 style={{fontSize:'8vmin'}} >Page Not Found</h1>
         <h1>ERROR 404</h1>
-        <Link to={'/RestaurentMenu'}><button className='btn btn-warning'>Back to Home</button></Link>
+        <Link to={'/'}><button className='btn btn-warning'>Back to Home</button></Link>
         </div>
       </center>
     </div>
